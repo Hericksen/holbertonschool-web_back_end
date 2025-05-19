6029dca8e9b953d44d55a20f812fee165d2fd09e
Hericksen <gwendal.martin35@hotmail.fr>
2025-05-19 11:41:19 +0200

    Refactor stdin.js to improve input handling and output formatting

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding("utf-8");
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
 
-process.stdin.on("data", (data) => {
-  const name = data.trim();
-  console.log(`Your name is: ${name}`);
-  process.stdin.end();
+  if (data) {
+    process.stdout.write(`Your name is: ${data}`);
+  }
 });
 
-process.stdin.on('end', () =>{
-  console.log("This important software is now closing");
-})
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
