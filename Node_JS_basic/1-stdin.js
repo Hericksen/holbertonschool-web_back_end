2cbc1b97b5fe2b54b0c7f138aa77c72338743d51
Hericksen <gwendal.martin35@hotmail.fr>
2025-05-19 11:23:52 +0200

    Fix event listener in stdin.js to correctly handle 'end' event instead of 'exit'

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -8,6 +8,6 @@ process.stdin.on("data", (data) => {
   process.stdin.end();
 });
 
-process.stdin.on('exit', () =>{
+process.stdin.on('end', () =>{
   console.log("This important software is now closing");
 })
